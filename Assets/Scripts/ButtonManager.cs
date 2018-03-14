94b79ffdb7f1ab72ba7c9f1894b05f3faee7e76d
unknown <sonieldaniel14@gmail.com>
2018-03-14 19:15:29 +0800

    Final Project OSS

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour 
+{
+	
+
+	public void LogoutBtn(string Login)
+	{
+		SceneManager.LoadScene (Login);
+	}
+
+	public void ExitBtn()
+	{
+		Application.Quit();
+	}
+
+	public void CreateBtn(string BrawlMain)
+	{
+		SceneManager.LoadScene (BrawlMain);
+	}
+
+	public void ConnectBtn(string Connect)
+	{
+		SceneManager.LoadScene (Connect);
+	}
+
+	public void LoginBtn(string Login)
+	{
+		SceneManager.LoadScene (Login);
+	}
+
+	public void CreateAccBtn(string Create)
+	{
+		SceneManager.LoadScene (Create);
+	}
+		
+
+}
---
+++ Assets/Scripts/UiAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiAnimation : MonoBehaviour {
+
+	public void DisableBoolAnimator(Animator anim)
+	{
+		anim.SetBool ("IsDisplayed", false);
+	}
+	public void EnableBoolAnimator(Animator anim)
+	{
+		anim.SetBool ("IsDisplayed", true);
+	}
+
+	public void DisableBoolAnimator1(Animator anim)
+	{
+		anim.SetBool ("IsDisplayed1", false);
+	}
+	public void EnableBoolAnimator1(Animator anim)
+	{
+		anim.SetBool ("IsDisplayed1", true);
+	}
+}
